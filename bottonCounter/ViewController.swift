01bf72395a4d95ef5be872bcd92dd00cad16acc7
Yoocie <yu634ki.tohoku.med@gmail.com>
2019-09-27 11:18:01 +0900

    Initial Commit

---
+++ bottonCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bottonCounter
+//
+//  Created by Yuki Uchida on 2019/09/27.
+//  Copyright © 2019 Yuki Uchida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
